c2132e9ca8aa83986b12027c59fa28ac3a6bc9ad
Richard Všianský <richard.vsiansky@sap.com>
2023-11-20 14:47:36 +0100

    Add playground

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# vut-2022-react
-Examples of React projects for VUT 2022 React Presentation
+# vut-2023-react
+Examples of React projects for VUT 2023 React Presentation
---
+++ playground/index.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => null
+
+createRoot(document.getElementById("root")).render(
+    <App />);
